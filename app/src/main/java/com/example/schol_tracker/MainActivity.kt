b67128f0844aab9dc20b2957c9740424c106db0a
Garrett Erven <gwerven@gmail.com>
2020-10-18 10:20:48 -0400

    Final commit before making presentation video. Very basic bare bones feature set

--- app/src/main/java/com/example/schol_tracker/MainActivity.kt
+++ app/src/main/java/com/example/schol_tracker/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.schol_tracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Main actvity configured
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
